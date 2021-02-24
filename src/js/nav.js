96c04dc60657c3a6685ec7259886d6ad66629414
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-02-24 00:52:32 -0300

    update

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,7 +1,14 @@
 const menuButton = document.getElementById("menu-button")
 const menu = document.getElementById("menu")
+const header = document.getElementById("header")
+const article = document.getElementById("article")
 
-menuButton.addEventListener("click", () =>{
-   menu.classList.toggle("menu--show")
-   document.body.toggle("no-scroll")
+
+header.addEventListener("click", e =>{
+   if(e.target === menuButton || e.target.classList.contains("main-header--active")){
+      menu.classList.toggle("menu--active")
+      header.classList.toggle("main-header--active")
+      article.classList.toggle("article--active")
+      document.body.classList.toggle("no-scroll")
+   }
 })
