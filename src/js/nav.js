aa378f558ad756264f0ca56f378514cf2a031522
Mrs. Krabs <47456460+MasterKrab@users.noreply.github.com>
2021-02-25 01:24:21 -0300

    Mobile and desktop

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,14 +1,23 @@
 const menuButton = document.getElementById("menu-button")
 const menu = document.getElementById("menu")
-const header = document.getElementById("header")
+const bgMenu = document.getElementById("bg-menu")
 const article = document.getElementById("article")
 
 
 header.addEventListener("click", e =>{
-   if(e.target === menuButton || e.target.classList.contains("main-header--active")){
+   if(e.target === menuButton || e.target.classList.contains("background-menu--active")){
+      menuButton.classList.toggle("top-bar__icon-menu--active")
       menu.classList.toggle("menu--active")
-      header.classList.toggle("main-header--active")
-      article.classList.toggle("article--active")
+      bgMenu.classList.toggle("background-menu--active")
       document.body.classList.toggle("no-scroll")
+   }
+})
+
+article.addEventListener("click", e =>{
+   e.preventDefault()
+
+   if( e.target.classList.contains("bookmarks") || e.target.classList.contains("bookmarks__icon") || e.target.classList.contains("bookmarks__text")){
+      document.querySelector(".bookmarks__icon").classList.toggle("bookmarks__icon--active")
+      document.querySelector(".bookmarks__text").classList.toggle("bookmarks__text--active")
    }
 })
